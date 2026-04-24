bf915ee21d19412f28b9f2a4ffb7649362689220
mbhupesh678 <mbhupesh678@gmail.com>
2026-04-25 00:58:49 +0530

    Implement merge intervals function in Solution class

---
+++ Merge_interval.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def merge(self, intervals):
+        if not intervals:
+            return []
+        
+        intervals.sort(key=lambda x: x[0])
+        merged = [intervals[0]]
+        
+        for current in intervals[1:]:
+            prev = merged[-1]
+            
+            if current[0] <= prev[1]:  # overlap
+                prev[1] = max(prev[1], current[1])
+            else:
+                merged.append(current)
+        
+        return merged
